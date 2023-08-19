6f0583bf2f9c7e1264c28ec1f1fa7659b39264ee
SebastianSalas09 <juans.salas@konradlorenz.edu.co>
2023-08-19 12:57:51 -0500

    ejercicios

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
-﻿namespace HabilidadesDeProgramacioI
+﻿using System;
+using System.Timers;
+
+namespace HabilidadesDeProgramacioI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //comment457878
-                    for (int i = 0; i < 4; i++) { 
-            }
-                   Console.WriteLine("The indez is" + i);
+            //Ejercicio3
+
+
+            //Ejercicio2
+            Random aleatorio = new Random();
+            Console.WriteLine();
+            int resultado = aleatorio.Next(0, 200);
+            Console.WriteLine(resultado);   
+            double porcentaje = 1.3;
+            double num2 = (porcentaje * resultado);
+            Console.WriteLine("numero" + aleatorio);
+
+            double num3 = resultado + num2;
+            Console.WriteLine("numero aumentado a 30%:" + num3);
+
+
+
+
+            //Ejercicio1
+
+            Console.WriteLine("ingrese un numero ");
+            string numero = Console.ReadLine();
+            int num = int.Parse(numero);
+            Console.WriteLine("El numero ingresado es:" + (num + 1));
         }
+                   
     }
 }
