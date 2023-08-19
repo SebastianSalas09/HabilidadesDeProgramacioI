0919e336e0e10aad21b38aa1250dd06902ecdfb6
SebastianSalas09 <juans.salas@konradlorenz.edu.co>
2023-08-19 11:24:06 -0500

    add comment

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             //comment457878
                     for (int i = 0; i < args.Length; i++) { 
             }
-                   Console.WriteLine("Hello, World!");
+                   Console.WriteLine("The indez is" + i);
         }
     }
 }
